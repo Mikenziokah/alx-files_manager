caf09d2402dd030e7c1153b3b6565343dc240926
Mikenziokah <mikenziokah@gmail.com>
2024-06-17 00:48:31 -0700

    server

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,17 @@
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
-
 const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
-const router = (app) => {
-  const route = express.Router();
-  app.use(express.json());
-  app.use('/', route);
+const router = express.Router();
 
-  route.get('/status', (request, response) => AppController.getStatus(request, response));
-  route.get('/stats', (request, response) => AppController.getStats(request, response));
-  route.post('/users', (request, response) => UsersController.postNew(request, response));
-  route.get('/connect', (request, response) => AuthController.getConnect(request, response));
-  route.get('/disconnect', (request, response) => AuthController.getDisconnect(request, response));
-  route.get('/users/me', (request, response) => UsersController.getMe(request, response));
-  route.post('/files', (request, response) => FilesController.postUpload(request, response));
-  route.get('/files/:id', (request, response) => FilesController.getShow(request, response));
-  route.get('/files', (request, response) => FilesController.getIndex(request, response));
-  route.put('/files/:id/publish', (request, response) => FilesController.putPublish(request, response));
-  route.put('/files/:id/unpublish', (request, response) => FilesController.putUnpublish(request, response));
-  route.get('/files/:id/data', (request, response) => FilesController.getFile(request, response));
-};
-export default router;
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/users/me', UsersController.getMe);
+router.get('/disconnect', AuthController.getDisconnect);
+router.post('/files', FilesController.postUpload);
+
+module.exports = router;
